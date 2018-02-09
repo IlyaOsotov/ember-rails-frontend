c0c7ff8bf9616f0fe87f0a57a5e050aae3937838
Ilya Osotov <soten58@yandex.ru>
2018-02-10 02:13:39 +0300

    added creating

--- app/components/task-list.js
+++ app/components/task-list.js
@@ -1,4 +1,14 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+    store: Ember.inject.service(),
+
+    actions: {
+        createTask() {
+            let store = this.get('store');
+            let task = store.createRecord('task', { name: 'test' });
+            console.log(task);
+            task.save();
+        }
+    }
 });
