22ad0d5094c8f93bbe7a93cf9c71bfbcbc84933d
Ilya Osotov <soten58@yandex.ru>
2018-02-09 21:34:02 +0300

    the root route to tasks route

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,7 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-    model: function() {
-        return this.store.findAll('task');
+    beforeModel() {
+        this.replaceWith('tasks');
     }
 });
