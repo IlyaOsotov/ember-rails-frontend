e6815fae9351df9ae014449676ef3523e25ae62a
Ilya Osotov <soten58@yandex.ru>
2018-02-10 15:55:23 +0300

    added task update

--- app/components/task-item.js
+++ app/components/task-item.js
@@ -9,6 +9,26 @@ export default Component.extend({
             store.findRecord('task', taskId, { backgroundReload: false }).then(function (task) {
                 task.destroyRecord();
             });
+        },
+
+        updateTask() {
+            let store = this.get('store');
+            let name = this.get('task.name');
+            let time = this.get('task.time');
+            let id = this.get('task.id');
+            store.findRecord('task', id).then(function(task) {
+                task.set('name', name);
+                task.set('time', time);
+                task.save();
+            });
+            this.set('isShowingModal', false);
+        },
+
+        showModal() {
+            this.set('isShowingModal', true);
+        },
+        cancel() {
+            this.set('isShowingModal', false);
         }
     }
 });
