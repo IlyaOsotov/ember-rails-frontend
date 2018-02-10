82a970d2a46d56c90cf1369de673b8c7abef0cfd
Ilya Osotov <soten58@yandex.ru>
2018-02-10 14:00:04 +0300

    added modal

--- app/components/task-list.js
+++ app/components/task-list.js
@@ -4,11 +4,18 @@ export default Component.extend({
     store: Ember.inject.service(),
 
     actions: {
+        newTask() {
+            this.set('isShowingModal', true);
+        },
         createTask() {
             let store = this.get('store');
-            let task = store.createRecord('task', { name: 'test' });
-            console.log(task);
+            let name = this.get('taskName');
+            let task = store.createRecord('task', { name: name });
             task.save();
+            this.set('isShowingModal', false);
+        },
+        cancel() {
+            this.set('isShowingModal', false);
         }
     }
 });
